3d1d8ae5554ddc1ad3dca7e776ca1ddba6c63633
Tharika <89331974+tharika01@users.noreply.github.com>
2022-12-11 19:47:20 +0530

    Update MainActivity.kt

--- BooksApp/app/src/main/java/com/example/booksapp/MainActivity.kt
+++ BooksApp/app/src/main/java/com/example/booksapp/MainActivity.kt
@@ -10,6 +10,8 @@ import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    
+    //Declaring the variables 
     lateinit var signUP : Button
     lateinit var loginBtn : Button
     lateinit var emailID : EditText
@@ -19,35 +21,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //getting the id's of the views 
         signUP = findViewById(R.id.signUpButton)
         loginBtn = findViewById(R.id.loginButton)
         emailID = findViewById(R.id.editTextEmailAddress)
         passID = findViewById(R.id.editTextPassword)
 
+        //open signup page when sign up button is clicked
         signUP.setOnClickListener{
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
 
+        // creating instance of SQLite db
         val helper = DbHelper(applicationContext)
-        val db = helper.readableDatabase  //returns instance of sqlite db
+        val db = helper.readableDatabase  
 
+        //Login authorization
         loginBtn.setOnClickListener {
+            //getting email and password entered by the user
             var email = emailID.text.toString()
             var password = passID.text.toString()
+            //query to check if the user details exists in LOGIN table
             val rs = db.rawQuery("SELECT * FROM LOGIN where EMAIL =? AND PASSWORD =?", arrayOf(email, password)) //2nd arg is for WHERE clause
-            if(rs.moveToNext())
+            if(rs.moveToNext()) //Successful login then open the home page
             {
                 Toast.makeText(applicationContext, "Login Successful", Toast.LENGTH_LONG).show()
                 val intent = Intent(this, HomeActivity::class.java)
                 startActivity(intent)
             }
-            else
+            else//invalid data then display toast msg, don't do anything
             {
                 Toast.makeText(applicationContext, "Invalid", Toast.LENGTH_LONG).show()
             }
         }
-
-
     }
 }
